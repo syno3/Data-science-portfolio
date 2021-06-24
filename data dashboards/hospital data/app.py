e9dc0732e81ca391ab72e6ed87b087373f9d9923
syno3 <sinotechxvista@gmail.com>
2021-06-24 15:28:19 +0300

    'modify'

--- data dashboards/hospital data/app.py
+++ data dashboards/hospital data/app.py
@@ -137,26 +137,25 @@ scatter = px.scatter(scatter_df, x="Risk adjusted rate", y="Deaths & Readmission
                  hover_name="Counties", log_x=True, size_max=30)
 
 ## geoplot fig variable
+"""
+hospitals 
+counties
+lat
+lon
+sum_County 
+"""
+""" location = df['Location ']
+lat = []
+lon = []
+print(location[0]) """
 """ 
-us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
-
-fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],color_discrete_sequence=["fuchsia"], zoom=3, height=300)
-fig.update_layout(
-    mapbox_style="white-bg",
-    mapbox_layers=[
-        {
-            "below": 'traces',
-            "sourcetype": "raster",
-            "sourceattribution": "United States Geological Survey",
-            "source": [
-                "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
-            ]
-        }
-      ])
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+geo = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+geo.update_layout(mapbox_style="open-street-map")
+geo.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 """
+
 ### setting the layout for the app
 
 app.layout = dbc.Container([
