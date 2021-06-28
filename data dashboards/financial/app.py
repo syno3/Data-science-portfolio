f7bb0a2e5fdfa39af28ddc2fe3a99a4859648c96
syno3 <sinotechxvista@gmail.com>
2021-06-28 15:15:04 +0300

    'added-updates'

--- data dashboards/financial/app.py
+++ data dashboards/financial/app.py
@@ -21,8 +21,7 @@ try:
     #plotly modules
     import plotly.express as px 
     #some other libraries that will be used 
-    from yahoo_fin.stock_info import *
-    import yfinance as yf
+
 
 except Exception as e:
     print(e)
@@ -30,15 +29,79 @@ except Exception as e:
 
 
 ##SETTING THE BOOSTRAP THEME AND TITLE OF WEBSITE
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 app.title = "Live market: S&P live market data"
 
-### DOING SOME WEIRD SHIT
 
 
 
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "border-right": "1px solid white"
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("Live market", className="display-3 title"),
+        html.Hr(),
+        html.P(
+            "We visualize live market data with dash", className="lead"
+        ),
+        ### CARD SECTION
+        dbc.Card(
+                [
+                    dbc.CardBody(
+                        [
+
+                        ]
+                    ),
+                ], className='sidebar-card'
+            )
+            ### END OF CARD SEECTION
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div([
+    dbc.Row([
+        dbc.Card(
+                [
+                    dbc.CardBody(
+                        [
+
+                        ]
+                    ),
+                ], className='card'
+            )
+        ]),
+    dbc.Row([
+        dbc.Card(
+                [
+                    dbc.CardBody(
+                        [
+                        ]
+                    ),
+                ],className='card'
+            )
+        ]),
+],style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
 
 ### WE INITIALIZE THE APP
 if __name__ == '__main__':
-    pass
+    app.run_server(debug=True)
