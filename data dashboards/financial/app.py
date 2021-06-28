d54b628c7c498dfb9dadbd13c10201e15204ca97
syno3 <sinotechxvista@gmail.com>
2021-06-28 13:50:18 +0300

    'updates-being-pushed'

--- data dashboards/financial/app.py
+++ data dashboards/financial/app.py
@@ -6,3 +6,39 @@ we visualize live stock market data and also get live news
 2. live news data
 
 """
+#### IMPORTING REQUIRED MODULES
+try:
+    #data analysis modules 
+    import pandas as pd
+    import numpy as np
+    #dash modules
+    import dash
+    import dash_core_components as dcc
+    import dash_html_components as html
+    import dash_bootstrap_components as dbc
+    from dash.dependencies import Output, Input
+    import plotly.graph_objs as go
+    #plotly modules
+    import plotly.express as px 
+    #some other libraries that will be used 
+    from yahoo_fin.stock_info import *
+    import yfinance as yf
+
+except Exception as e:
+    print(e)
+
+
+
+##SETTING THE BOOSTRAP THEME AND TITLE OF WEBSITE
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+app.title = "Live market: S&P live market data"
+
+### DOING SOME WEIRD SHIT
+
+
+
+
+
+### WE INITIALIZE THE APP
+if __name__ == '__main__':
+    pass
