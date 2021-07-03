d8d44d5a9c2a1e73ad067c5d460b22231908b30a
syno3 <sinotechxvista@gmail.com>
2021-07-03 20:18:43 +0300

    'modify'

---
+++ data dashboards/uber/app.py
@@ -0,0 +1,18 @@
+try:
+    import pandas as pd
+    import numpy as np
+
+except Exception as e:
+    print(e)
+
+
+class Dashboard:
+    def __init__(self, data):
+        self.data = data
+        
+    def DataProcessing(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
